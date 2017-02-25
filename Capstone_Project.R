27a2bb17c6dba54c7f0e0307a92d873eb460d501
Mike Crabtree <Mike.D.Crabtree@gmail.com>
2017-02-25 14:33:37 -0500

    Comments and Names
    Inserted section comments and named plots

--- Capstone_Project.R
+++ Capstone_Project.R
@@ -7,10 +7,12 @@ library(qdap)
 library(RWeka)
 library(ggplot2)
 
+#Set file path
 news <- "./en_US.news.txt"
 blogs <- "./en_US.blogs.txt" 
 twitter <- "./en_US.twitter.txt"
 
+#Import data
 news_data <- scan(news, character(0), sep = "\n", skipNul = TRUE)
 news_lines <- as.numeric(countLines(news))
 
@@ -20,10 +22,12 @@ blogs_lines <- as.numeric(countLines(blogs))
 twitter_data <- scan(twitter, character(0), sep = "\n", skipNul = TRUE)
 twitter_lines <- as.numeric(countLines(twitter))
 
+#Print line counts
 print(paste("The line count for the News data is: ", news_lines))
 print(paste("The line count for the Blogs data is: ", blogs_lines))
 print(paste("The line count for the Twitter data is: ", twitter_lines))
 
+#Sample data
 news_con <- file("./en_US.news.txt", "r") 
 news_sample <-readLines(news_con,
                         (news_lines/1500),encoding= "UTF-16LE")
@@ -42,11 +46,14 @@ twitter_sample <- readLines(twitter_con,
 writeLines(twitter_sample, con="datatwa.txt", "\n")
 close(twitter_con)
 
+#Merge data
 data_all <- paste(blog_sample,news_sample,twitter_sample)  
 data_all <- sent_detect(data_all, language = "en", model = NULL)
 
+#Create corpus
 corpus <- VCorpus(VectorSource(data_all))
 
+#Data cleaning
 corpus <- tm_map(corpus, content_transformer(tolower)) 
 corpus <- tm_map(corpus, content_transformer(removePunctuation))
 corpus <- tm_map(corpus, content_transformer(removeNumbers))
@@ -57,10 +64,12 @@ url_fun <- function(x){
 }
 corpus <- tm_map(corpus, content_transformer(url_fun))
 
+#Profanity filtering
 profanity <- readLines("./bad-words.txt")
 
 corpus <- tm_map(corpus, removeWords, profanity) 
 
+#Tokenization
 onegram <- function(x) {
   NGramTokenizer(x, Weka_control(min = 1, max = 1))
 }
@@ -71,6 +80,7 @@ trigram <- function(x) {
   NGramTokenizer(x, Weka_control(min = 3, max = 3))
 }
 
+#Document matrices
 doc_onegram <- DocumentTermMatrix(corpus
                                   ,control=list(tokenize=onegram))
 doc_bigram  <- DocumentTermMatrix(corpus
@@ -85,21 +95,22 @@ bigram_sort <- sort(colSums(as.matrix(doc_bigram))
 trigram_sort <- sort(colSums(as.matrix(doc_trigram))
                      ,decreasing=TRUE)
 
-barplot(onegram_sort[1:25],
+#Histograms of n-gram
+onegram_hist <- barplot(onegram_sort[1:25],
         ylab='Frequency',
         xlab='Word',
         main='Top 25 Most Frequently Used Words',
         names.arg=names(onegram_sort)[1:25],        
         col="blue", las=2, cex.names=.7)
 
-barplot(bigram_sort[1:25],
+twogram_hist <- barplot(bigram_sort[1:25],
         ylab='Frequency',
         xlab='Phrase',
         main='Top 25 Most Frequently Used 2-word Phrases',
         names.arg=names(bigram_sort)[1:25],        
         col="green", las=2, cex.names=.7)
 
-barplot(trigram_sort[1:25],
+threegram_hist <- barplot(trigram_sort[1:25],
         ylab='Frequency',
         xlab='Phrase',
         main='Top 25 Most Frequently Used 3-word Phrases',
